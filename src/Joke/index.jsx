cff23ef7a2b0344954bec457a230004ce653c6b9
Tereza Sudíková <tereza.sudik@gmail.com>
2021-05-22 19:30:17 +0200

    ukol 15 nefunkcni

--- src/Joke/index.jsx
+++ src/Joke/index.jsx
@@ -1,7 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './style.css';
 
 //pět props: userAvatar, userName, text, likes, dislikes
-const Joke = (props) => {};
+const Joke = (props) => {
+  const [scoreLike, setScoreLike] = useState(0);
+  const [scoreDislike, setScoreDislike] = useState(0);
+
+  const handleClickLike = () => {
+    setScoreLike(scoreLike + 1);
+  };
+
+  const handleClickDislike = () => {
+    setScoreDislike(scoreDislike + 1);
+  };
+
+  return (
+    <>
+      <div className="joke">
+        <div className="joke__body">
+          <div className="joke__user">
+            <img className="user-avatar" src={props.userAvatar} />
+            <p className="user-name">{props.userName}</p>
+          </div>
+
+          <p className="joke__text">{props.text}</p>
+        </div>
+        <div className="joke__likes">
+          <button
+            onClick={handleClickLike}
+            id="btn-up"
+            className="btn-like btn-like--up"
+          ></button>
+          <span id="likes-up" className="likes-count likes-count--up">
+            {scoreLike}
+            {props.likes}
+          </span>
+          <button
+            onClick={handleClickDislike}
+            id="btn-down"
+            className="btn-like btn-like--down"
+          ></button>
+          <span id="likes-down" className="likes-count likes-count--down">
+            {scoreDislike}
+            {props.dislikes}
+          </span>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default Joke;
